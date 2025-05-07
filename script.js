051d55f177a9ba00880a5999447c4cff05c6988c
acemadapaker <braunkun21ao@gmail.com>
2025-05-07 11:54:14 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function scrollToContact() {
+    document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
+  }
+  
+  // You can also add future enhancements or toast notifications here.
+  
